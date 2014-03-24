5432eb40ba3609d8575067b2631e02e4f6298963
hamza0088 <hamzarazzak@hamzas-MacBook-Pro.local>
2014-03-24 19:26:24 +0000

    update. added javascript module step

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,17 +1,20 @@
 Given(/^User lands on homepage$/) do
+  visit('file:///Users/hamzarazzak/Desktop/index.html') 
+  sleep 5
+  page.has_xpath?("html/body/form/center/input[2]")
 end
 
 When(/^User fills in "(.*?)" and "(.*?)"$/) do |username, password|
-  fill_in'Username:',with: username
-  fill_in'password', with: password
+  find(:xpath, "html/body/form/center/input[1]").set username
+  find(:xpath, "html/body/form/center/input[2]").set password
 end
 
-	
-
 When(/^User presses login$/) do
-  pending # express the regexp above with the code you wish you had
+  sleep 5
+  click_button('Login')
+  page.driver.browser.switch_to.alert.accept
 end
 
 Then(/^User is directed to success page$/) do
-  pending # express the regexp above with the code you wish you had
+    page.has_content?('You have successfully entered a correct username and password')  
 end
